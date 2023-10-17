6b8eca337bae966a13604a75eb03246e7b7020c7
Profxbaba <daddyslove41@gmail.com>
2023-10-17 10:15:27 -0700

    get_precision

--- get_precision.c
+++ get_precision.c
@@ -15,27 +15,7 @@ int get_precision(const char *format, int *i, va_list list)
 
 	if (format[curr_i] != '.')
 		return (precision);
-}
 
-#include "main.h"
-
-/************************* WRITE HANDLE *************************/
-/**
- * handle_write_char - Prints a string
- * @c: char types.
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags.
- * @width: get width.
- * @precision: precision specifier
- * @size: Size specifier
- *
- * Return: Number of chars printed.
- */
-int handle_write_char(char c, char buffer[],
-	int flags, int width, int precision, int size)
-{ /* char is stored at left and paddind at buffer's right */
-	int i = 0;
-	char padd = ' ';
 	precision = 0;
 
 	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
